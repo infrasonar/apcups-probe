ed0f28bf9913f0857166cca03de2087445a0b714
Koos85 <koosjoosten@gmail.com>
2024-09-18 17:04:25 +0200

    snmpclient and snmpquery

---
+++ lib/snmpquery.py
@@ -0,0 +1,37 @@
+import logging
+from asyncsnmplib.client import Snmp, SnmpV1, SnmpV3
+from asyncsnmplib.exceptions import SnmpNoAuthParams
+from asyncsnmplib.exceptions import SnmpNoConnection
+from asyncsnmplib.mib.utils import on_result_base
+from libprobe.exceptions import CheckException
+from typing import Union, Tuple, Dict, List, Any
+
+
+async def snmpquery(
+    client: Union[Snmp, SnmpV1, SnmpV3],
+    oids: Tuple[Tuple[int], ...]
+) -> Dict[str, List[Dict[str, Any]]]:
+
+    try:
+        await client.connect()
+    except SnmpNoConnection:
+        raise
+    except SnmpNoAuthParams:
+        logging.warning('unable to connect: failed to set auth params')
+        raise
+    else:
+        results = {}
+        for oid in oids:
+            result = await client.walk(oid)
+            try:
+                name, result = on_result_base(oid, result)
+            except Exception as e:
+                msg = str(e) or type(e).__name__
+                raise CheckException(
+                    f'Failed to parse result. Exception: {msg}')
+            else:
+                results[name] = result
+        return results
+    finally:
+        # safe to close whatever the connection status is
+        client.close()
